5b50b00dd73857a41738fe7e980c1e8d5013835c
Youvprog <71141047+Youvprog@users.noreply.github.com>
2022-08-28 23:29:44 +0400

    updating urls.py

--- Shop/urls.py
+++ Shop/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include("Store.urls")),
     path('',include("accounts.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+ + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
